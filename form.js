28124e874c7f8f60a1db8cc72a3760b03883534f
natwoj <info@nwojcik.com>
2022-02-25 13:41:08 +0100

    basic email validate with regexp

--- form.js
+++ form.js
@@ -1,27 +1,20 @@
-const inputName = document.getElementById("form-name")
-const inputSurname =  document.getElementById("form-surname")
-const inputEmail =  document.getElementById("form-email")
+const form = document.querySelector("form");
+const inputName = document.getElementById('form-name')
+const inputSurname = document.getElementById('form-surname')
 const inputPhone =  document.getElementById("form-phone")
 const textArea = document.getElementById("form-msg")
 const formButton = document.querySelector("button")
+const inputEmail =  document.getElementById("form-email") 
 
-// podstawowa walidacja emaila // 
-const validateEmailRegex = /^\S+@\S+\.\S+$/;
-const result = validateEmailRegex.test("natalia@gmasilcom");
 
-function EmailValidate (inputEmail) {
-if (validateEmailRegex.test(inputEmail.value)) {
-    console.log ("ok")
+///------------BASIC EMAIL VALIDATION FUNCTION, dots at the end and at the beginning not allowed ----------///
+
+function validateEmail(inputEmail) {
+const validateEmailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+const result = validateEmailRegex.test(inputEmail);
+if (result) {
+    return result;
 }
-else console.log("niepoprawny")
+else return false;
 }
-/* test */
-EmailValidate("natalia@gmasilcom");
-
-// Sprawdzenie czy wszystkie pola zostały wypełnione 
-formButton.addEventListener("click", EmailValidate(inputEmail));
-*/
-
-
-
-
+console.log(validateEmail("..natalia@gmail.com"))
